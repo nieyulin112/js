ef9242ea99e0b5dd002ea8062119449f6ce301d8
ppd-03020225 <ppd-03020225@ppd-03020225deMacBook-Pro.local>
2018-10-25 13:51:08 +0800

    js

--- recursive.js
+++ recursive.js
@@ -14,7 +14,7 @@ function facior(n, result = 1) {
     return result
   } else {
     let p = n * result
-    return facior(n - 1 , p)
+    return facior(n - 1, p)
   }
 }
 
@@ -26,3 +26,19 @@ function fib(n) {
     return fib(n - 1) + fib(n - 2)
   }
 }
+
+// 优化的斐波那契数列
+var cache = [];
+
+function fibs(n) {
+  if (cache[n] !== undefined) {
+    return cache[n];
+  }
+  if (n <= 2) {
+    cache[n] = 1;
+    return 1;
+  }
+  cache.push(fib(n - 1) + fib(n - 2));
+  return cache[n];
+}
+console.log(fib(5));
